5dce142e0316d67ec7fe4303f53511b1ec86df96
DarioDalte <dariodalte@gmail.com>
2022-01-04 18:17:30 +0100

    Resolved bug

--- api/user/user-info.php
+++ api/user/user-info.php
@@ -1,9 +1,8 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: access");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: GET");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header('Access-Control-Allow-Headers: *'); 
 
 require '../../config/database.php';
 require '../../middlewares/Auth.php';
